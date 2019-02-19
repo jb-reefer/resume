234ba75bfde970b70715c2bf36ce09f9276348ed
J.B. Reefer <jb@xdotais-MacBook-Pro-6.local>
2019-02-18 21:27:27 -0500

    Added hero

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import { ExperienceBlock, IExperienceBlock } from './components/ExperienceBlock'
 import { Entry } from './components/Entries';
 import data from './data';
 import './App.css'
+import { Hero } from './components/Hero';
 
 const App = (props: any) => (
   <div className="Container">
-  {data.map(job => (
-    <ExperienceBlock company={job.company} city={job.city} role={job.role} from={job.from} to={job.to}>
-      {job.children.map(entry => <Entry>{entry}</Entry>)}
-    </ExperienceBlock>
-  ))}
+    <Hero />
+    {data.map(job => (
+      <ExperienceBlock company={job.company} city={job.city} role={job.role} from={job.from} to={job.to}>
+        {job.children.map(entry => <Entry>{entry}</Entry>)}
+      </ExperienceBlock>
+    ))}
   </div>
 );
 
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Hero.css';
+
+export const Hero = (props: any) => (
+  <div className="Hero">
+    
+    <h1>Senior/Lead Full Stack Engineer</h1>
+    <address>New York, NY 10128</address>
+    <span>(573) 308-2658    |    james.reefer@gmail.com</span>
+  </div>
+)
