69cbbec9aa181d14bb19a6bc9c21c737e80eb01a
J.B. Reefer <jb@xdotais-MacBook-Pro-6.local>
2019-02-18 22:54:58 -0500

    layout with github logo

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -4,8 +4,12 @@ import './Hero.css';
 export const Hero = (props: any) => (
   <address className="Hero">
     <h1 title="Really it's James Brooks Gardner Reefer but this is so much simpler">J.B. Reefer</h1>
-    <div>Senior/Lead Full Stack Engineer</div>
+    {/* <div>Senior/Lead Full Stack Engineer</div> */}
     <div>New York, NY 10128</div>
-    <span>(573) 308-2658 | <a href="mailto:james.reefer@gmail.com?subject=Let's be friends!">Email</a></span>
+    <div>(573) 308-2658</div>
+    <div className="Contacts">
+      <a href="https://github.com/jb-reefer"><img height="24" src="/GitHub-Mark-64px.png" /></a>|
+      <a href="mailto:james.reefer@gmail.com?subject=Let's be friends!">Email</a>
+    </div>
   </address>
 )
