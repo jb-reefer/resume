68f093d71a0331166e7ecb28c9df451c93ecbce0
jb-reefer <james.reefer@gmail.com>
2019-02-25 23:08:42 -0500

    added fork me

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import { ExperienceBlock, IExperienceBlock } from './components/ExperienceBlock';
-import { Entry } from './components/Entries';
-import data from './data';
-import './App.css'
-import { Hero } from './components/Hero';
-import { Education } from './components/Education';
+import React from "react";
+import "./App.css";
+import { Education } from "./components/Education";
+import { Entry } from "./components/Entries";
+import { ExperienceBlock } from "./components/ExperienceBlock";
+import { GithubButton } from "./components/GithubButton";
+import { Hero } from "./components/Hero";
+import data from "./data";
 
 const App = (props: any) => (
   <div className="Container">
     <Hero />
     <br />
-    {data.map(job => (
+    {data.map((job) => (
       <ExperienceBlock company={job.company} city={job.city} role={job.role} from={job.from} to={job.to}>
-        {job.children.map(entry => <Entry>{entry}</Entry>)}
+        {job.children.map((entry) => <Entry>{entry}</Entry>)}
       </ExperienceBlock>
     ))}
     <br />
     <Education />
+    <div className="ForkMe"><GithubButton link="https://github.com/jb-reefer/resume" /> source for this site :)</div>
   </div>
 );
 
